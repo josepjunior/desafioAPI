d96679caa0d50f06cfe417032b6ad8d5648dd7a8
José Junior <josepsjunior.df@gmail.com>
2022-04-14 23:39:20 -0300

    Ajustes de usuario

---
+++ src/test/java/br/com/desafioapimongo/desafioapi/DesafioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafioapimongo.desafioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
